4a264238654617f02696fff88ac1e57f52e7f3f7
qazwsx6669 <120317923+qazwsx6669@users.noreply.github.com>
2024-10-09 18:35:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'OBITO BOT',
+  author: 'MADARA',
+  botName: "OBITO",
+  botOwner: 'MADARA', // Your name
+  ownerNumber: '01224078537', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
